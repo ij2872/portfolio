d9c2ada0524b2e807d71132e1b6014254a5b25fc
Ivan <ijaimes2872@gmail.com>
2017-10-11 15:55:52 -0500

    Fix resume button sends to resume page

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
 import './styles/main.scss';
+
+$(document).ready(function(){
+    $('.resume-btn').on('click', function(e){
+        e.preventDefault(); 
+        window.location.href = 'test.pdf';
+    })
+});
